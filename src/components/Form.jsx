6c03f62e1acc230aec36a45a23ec47503c10751b
Carmen-Trillo <mctrillon@gmail.com>
2023-02-14 15:59:07 +0100

    created API fake and database in json

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+export default function formAddProduct(){
+    return
+
+    <form class="formulario" method="POST">
+            <label>
+            Producto:
+            <input type="text" name="product" />
+            </label>
+    </form>
+}
