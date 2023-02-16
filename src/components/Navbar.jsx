b173346dedc18b049458afe98da974355f61ed37
miriamremesal <miriamremesal@gmail.com>
2023-02-16 14:48:54 +0100

    navbar and header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavbarHotBuy() {
+  return (
+    <>
+    <Navbar bg="light" >
+      <Container>
+        <Navbar.Brand href="#NavbarHotBuy"> <img alt="" src="../../src/assets/img/logo.png" width="30" height="30" className="d-inline-block align-top"/>{' '}
+            HOT BUY</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#uploadProduct">Subir producto</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar producto"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+      </Container>
+    </Navbar>
+    </>
+  );
+}
+
+export default NavbarHotBuy;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
+
+import NavbarHotBuy from './components/Navbar'
+// import HeaderHotBuy from './components/Header'
 import MyForm from './components/Form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Product />
+    <NavbarHotBuy />
+    {/* <HeaderHotBuy /> */}
+    {/* <Product /> */}
     <MyForm />
   </React.StrictMode>,
 )
