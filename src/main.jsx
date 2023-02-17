c621134c78d507a1ae30e6f0656032852f16afd0
Milacover <117834108+Milacover@users.noreply.github.com>
2023-02-17 10:59:09 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import StockCounter from './components/StockCounter'
 import './index.css'
 import MyForm from './components/Form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Product />
+    {/* <Product /> */}
+    <StockCounter/>
     <MyForm />
   </React.StrictMode>,
 )
