1257fdc07cc245488a558a099e2858308db9ef5c
Milacover <117834108+Milacover@users.noreply.github.com>
2023-02-19 12:21:19 +0100

    add feature/router

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import NavbarHotBuy from './components/Navbar'
-// import HeaderHotBuy from './components/Header'
+//import HeaderHotBuy from './components/Header'
 import MyForm from './components/Form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Product />
+    {/* <Product /> */}
+    < NavbarHotBuy/>
     <MyForm />
+    
   </React.StrictMode>,
 )
