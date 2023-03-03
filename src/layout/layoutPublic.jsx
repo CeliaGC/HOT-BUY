7982b20134c6bb6a3a70069e637104b767846f07
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-03 11:38:57 +0100

    vbv

--- src/layout/layoutPublic.jsx
+++ src/layout/layoutPublic.jsx
@@ -1,6 +1,5 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Products from "../pages/Products";;
 import Footer from "../components/Footer";
 
 const LayoutPublic = () => {
