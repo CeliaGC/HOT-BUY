9bac9ae322a429d9517861f371b6ae47e0165062
Carmen-Trillo <mctrillon@gmail.com>
2023-03-03 10:47:14 +0100

    Configured responsive view with js function

--- src/components/DetailView.jsx
+++ src/components/DetailView.jsx
@@ -6,22 +6,13 @@ import StockCounter from '../components/StockCounter'
 import './DetailView.css'
 import '../../src/index.css'
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import CardHeader from '@mui/material/CardHeader';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-
 
 
 export default function DetailedView() {
@@ -48,37 +39,38 @@ export default function DetailedView() {
     let year = newDate.getFullYear();
     let dateUploaded = `${day}/${month}/${year}`;
 
- 
-
-  return (
-    <div id='card'>
-        <Card sx={{ maxWidth: '80vw', margin: 'auto' }}>
-      <CardActionArea style={{display: 'flex', flexDirection: 'row'}}>
+    function mediaquery(){
+      let mediaqueryList = window.matchMedia("(max-width: 760px)");
+      if (mediaqueryList.matches){
+        return(
+        <div>
+        <Card sx={{ maxWidth: '95vw', margin: 'auto' }}>
+      <CardActionArea style={{display: 'flex', flexDirection: 'column'}}>
         <div id='imgProdct'>
-        <CardMedia style={{width:'20vw', marginRight: '3rem', padding: '1rem'}}
+        <CardMedia style={{width:'60vw', margin:'auto', padding: '1rem'}}
           component="img"
           image={img}
           alt={name}
         /></div>
         <div>
-        <CardContent style={{width:'50vw'}}>
-          <Typography style={{backgroundColor:'#7C2E9B', borderRadius: '5px', height: '2.4rem'}} gutterBottom variant="h5" component="div">
-          <h2>{name}</h2>
+        <CardContent style={{width:'80vw'}}>
+          <Typography style={{backgroundColor:'#7C2E9B', borderRadius: '5px', height: '2.4rem', fontSize:'10px'}} gutterBottom variant="h2" component="div">
+          <h3>{name}</h3>
           </Typography>
           <Typography gutterBottom variant="h6" component="div">
           <p>{dateUploaded}</p>
           </Typography>
           <Typography gutterBottom variant="h6" component="div">
-          <h4>Categoría del producto: {category}</h4>
+          <p><strong><font color="#7C2E9B">Categoría del producto:</font></strong> {category}</p>
           </Typography>
           <Typography gutterBottom variant="h6" component="div">
-          <h4>Estado del producto: {productStatus}</h4>
+          <p><strong><font color="#7C2E9B">Estado del producto:</font></strong> {productStatus}</p>
           </Typography>
           <Typography  variant="body1" color="text.secondary">
           <p>{description}</p>
           </Typography>
           <Typography gutterBottom variant="h5" component="div">
-          <p>Precio: {price}€</p>
+          <p><strong><font color="#7C2E9B">Precio:</font></strong> {price}€</p>
           </Typography>
           <Typography>
             <StockCounter />
@@ -92,9 +84,60 @@ export default function DetailedView() {
         </Button>
       </CardActions>
     </Card>
-    <div id='buttonBack'>
-        <Link to="/" className="nav--item"><input id="back" type="button" value="VOLVER" /></Link>
     </div>
+    )}else{
+      return(
+      <div>
+        <Card sx={{ maxWidth: '80vw', margin: 'auto' }}>
+        <CardActionArea style={{display: 'flex', flexDirection: 'row'}}>
+          <div id='imgProdct'>
+          <CardMedia style={{width:'20vw', marginRight: '3rem', padding: '1rem'}}
+            component="img"
+            image={img}
+            alt={name}
+          /></div>
+          <div>
+          <CardContent style={{width:'50vw'}}>
+            <Typography style={{backgroundColor:'#7C2E9B', borderRadius: '5px', height: '2.4rem'}} gutterBottom variant="h5" component="div">
+            <h2>{name}</h2>
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+            <p>{dateUploaded}</p>
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+            <h4><strong><font color="#7C2E9B">Categoría del producto:</font></strong> {category}</h4>
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+            <h4><strong><font color="#7C2E9B">Estado del producto:</font></strong> {productStatus}</h4>
+            </Typography>
+            <Typography  variant="body1" color="text.secondary">
+            <p>{description}</p>
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+            <p><strong><font color="#7C2E9B">Precio:</font></strong> {price}€</p>
+            </Typography>
+            <Typography>
+              <StockCounter />
+          </Typography>
+          </CardContent>
+          </div>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+      </div>
+      )}
+    }
+
+  return (
+    <div id='card'>
+      {mediaquery()}
+      <div id='buttonBack'>
+          <Link to="/Products" className="nav--item"><input id="back" type="button" value="VOLVER" /></Link>
+      </div>
     </div>
     );
 }
--- src/components/StockCounter.jsx
+++ src/components/StockCounter.jsx
@@ -30,10 +30,10 @@ function StockCounter() {
   return (
     <>
       <div>
-        <p>Unidades: {unit}</p>
-        <button style={{fontFamily: 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)'}} onClick={() => setUnit(0)}>Sin stock</button>
-        <button style={{fontFamily : 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)', marginLeft: '0.5rem', marginRight: '0.5rem'}}onClick={() => setUnit(prevCount => prevCount - 1)}>-</button>
-        <button style={{fontFamily: 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)'}} onClick={() => setUnit(prevCount => prevCount + 1)}>+</button>
+        <p><strong><font color="#7C2E9B">Unidades:</font></strong> {unit}</p>
+        <button style={{fontFamily: 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)', border: 'none', padding: '0.7rem'}} onClick={() => setUnit(0)}>Sin stock</button>
+        <button style={{fontFamily : 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)', border: 'none', marginLeft: '0.5rem', padding: '0.7rem', marginRight: '0.5rem'}}onClick={() => setUnit(prevCount => prevCount - 1)}>-</button>
+        <button style={{fontFamily: 'Goldman', fontWeight: 'bold', backgroundColor:'rgba(215, 201, 240, 1)', border: 'none', padding: '0.7rem'}} onClick={() => setUnit(prevCount => prevCount + 1)}>+</button>
       </div>
       <form>
       <Button onClick={handleSubmit} variant="contained" style={{margin: 'auto', backgroundColor:'#D7C9F0', color:'#7C2E9B'}}><UpdateIcon /></Button>
